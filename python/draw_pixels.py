67914043d9f797904d7b74f8736f8a1cfc9a6ba7
Goodsome <382009663@qq.com>
2017-12-19 20:43:40 +0800

    pixels

--- python/draw_pixels.py
+++ python/draw_pixels.py
@@ -1,5 +1,4 @@
 from OpenGL.GL import *
-from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import numpy as np
 import read
@@ -17,21 +16,35 @@ zoom = np.max((v_max - v_min)[0:2] / [width, height])
 vertex -= v_min
 vertex /= zoom
 
-v = vertex[indices]
+tri_v = vertex[indices]
+act_v = np.array([])
+act_l = np.array([])
 colors = np.full(indices.shape, 0.07)
 zbuffer = np.zeros(width)
 pixels = np.zeros((height, width, 3))
 
-a = v[:, :, 1]
-b = np.argsort(a, axis=1)
-for i in range(v.shape[0]):
-    v[i] = v[i, b[i,::-1]]
-c = v[:, 0, 1]
-d = np.argsort(c)
-v = v[d[::-1]]
+for i in range(tri_v.shape[0]):
+    tri_v[i] = tri_v[i, np.argsort(tri_v[:, :, 1], axis=1)[i, ::-1]]
+tri_v = tri_v[np.argsort(tri_v[:, 0, 1])[::-1]]
+
+for i in range(int(max(vertex[:, 1])), -1, -1):
+    tran = (tri_v[:, 0, 1] > i) * (tri_v[:, 0, 1] < i + 1)
+    act_v = np.append(act_v, tri_v[tran]).reshape(-1, 3, 3)
+    act_v = act_v[act_v[:, 2, 1] < i]
+    act_v1 = act_v[act_v[:, 1, 1] < i]
+    act_v2 = act_v[act_v[:, 1, 1] > i]
+    print(i)
+    for j in range(int(np.min(act_v[:, :, 0])), int(np.max(act_v[:, :, 0])) + 1):
+        # dx = (act_v1[:, 0, 0] - act_v1[:, 2, 0]) / (act_v1[:, 0, 1] - act_v1[:, 2, 1])
+        # tran_l = np.ceil(dx * (i - act_v1[:, 0, 1]) + act_v1[:, 0, 0]) == j
+        # act_l = np.append(act_l, act_v1[tran_l])
+        pass
+
+print(act_v.shape)
 
 
 def display():
+
     global pixels
 
     glClear(GL_COLOR_BUFFER_BIT)
@@ -39,13 +52,16 @@ def display():
     glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels)
     glutSwapBuffers()
 
+
 def keyboard(*args):
     if args:
         exit()
 
-def initGL():
+
+def init():
 
     glClearColor(0, 0, 0, 0)
+
 
 def main():
 
@@ -55,7 +71,7 @@ def main():
     glutCreateWindow("Draw Pixels")
     glutDisplayFunc(display)
     glutKeyboardFunc(keyboard)
-    initGL()
+    init()
     glutMainLoop()
 
 # main()
